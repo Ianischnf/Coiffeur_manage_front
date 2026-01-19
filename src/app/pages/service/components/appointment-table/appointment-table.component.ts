0cb92726743f665ede6a559c806f69bb90482b8b
Ianischnf <81810701+Ianischnf@users.noreply.github.com>
2026-01-19 12:30:16 +0100

    recuperation rdv data tables

--- src/app/pages/service/components/appointment-table/appointment-table.component.ts
+++ src/app/pages/service/components/appointment-table/appointment-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { Appointment, AppointmentService } from 'src/app/services/appointment.service';
 
@@ -9,18 +9,7 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+let ELEMENT_DATA: PeriodicElement[] = [];
 
 @Component({
   selector: 'app-appointment-table',
@@ -29,7 +18,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 
-export class AppointmentTableComponent {
+export class AppointmentTableComponent implements OnInit {
 
   constructor(private appointmentService : AppointmentService){}
 
@@ -39,7 +28,7 @@ export class AppointmentTableComponent {
 
   appointments : Appointment[] = [];
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['client', 'description', 'status', "date"];
   dataSource = ELEMENT_DATA;
   clickedRows = new Set<PeriodicElement>();
 
@@ -47,7 +36,7 @@ export class AppointmentTableComponent {
     this.appointmentService.fetchAllAppointmentHairdresser().subscribe({
       next: (res) => {
         this.appointments = res
-        console.log('RDV : ', res);
+        console.log('RDV : ', this.appointments);
       },
       error: (err) => {
         console.log("Erreur", err);
